71733aed36ba4e2695908ff4c3cb7d09ec3d7e83
Zach Posten <zach@posten.io>
2020-06-26 01:03:58 -0500

    feat(lib): Use new vanilla API

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {TintedWheel} from 'react-tinted'
 
 import './App.scss'
-import {TintedPalette} from 'react-tinted'
+import {TintedPalette, colorModes} from 'react-tinted'
 
 function App() {
+  const [mode, setMode] = useState(colorModes.ANALOGOUS)
+
   return (
-    <TintedWheel>
-      <TintedPalette />
-    </TintedWheel>
+    <div className='my-color-wheel tinted--dark'>
+      <TintedWheel colorCount={5} mode={mode}>
+        <TintedPalette />
+      </TintedWheel>
+
+      <select
+        onChange={e => setMode(e.target.value)}
+        className='my-mode-picker'
+      >
+        {Object.values(colorModes).map(mode => (
+          <option value={mode}>{mode}</option>
+        ))}
+      </select>
+    </div>
   )
 }
 
